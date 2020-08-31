f1de03af7bb1db29807caee81fa9eb01e457aa22
mariamoljames <mariamol.james@gmail.com>
2020-08-31 15:43:27 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -24,23 +24,32 @@ class App extends React.Component {
     }
     console.log("createTodo", newTodo)
 
-    // TODO: add the new Todo to the list in state
-    // make sure not to mutate the existing array!
+      let updatedTodos = [...this.state.todos, newTodo]
+      this.setState({todos: updatedTodos})
+
   }
 
   deleteTodo = id => {
     console.log("deleteTodo", id)
-
-    // TODO: remove ONE todo from state using the id
-    // make sure not to mutate the existing array!
+    
+    let updatedTodos = this.state.todos.filter(todo => todo.id !== id)
+    this.setState({todos: updatedTodos})
   }
 
   updateTodo = (id, completed) => {
     console.log("updateTodo", id, completed)
 
-    // TODO: update ONE todo in state using the id
-    // make sure not to mutate the existing array!
-    // also, make sure not to mutate any objects within the array!
+    let updatedTodos = this.state.todos.map(todo => {
+      if (todo.id === id){
+        let updatedToDo = {...todo}
+        updatedToDo.completed = true
+        return updatedToDo
+      }else{
+        return todo
+      }
+    })
+
+    this.setState({todos: updatedTodos})
   }
 
   render() {
